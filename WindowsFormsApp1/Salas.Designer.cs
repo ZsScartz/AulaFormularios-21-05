719c44746d02e49aef794e196309c47490dc813d
Aluno <Aluno@LS05M07>
2024-05-21 09:39:51 -0300

    Add project files.

---
+++ WindowsFormsApp1/Salas.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Salas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumPC = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblNumPC = new System.Windows.Forms.Label();
+            this.chkLab = new System.Windows.Forms.CheckBox();
+            this.lblSalas = new System.Windows.Forms.Label();
+            this.txtNumCadeiras = new System.Windows.Forms.TextBox();
+            this.lblNumCadeiras = new System.Windows.Forms.Label();
+            this.txtPredio = new System.Windows.Forms.TextBox();
+            this.lblPredio = new System.Windows.Forms.Label();
+            this.txtComplemento = new System.Windows.Forms.TextBox();
+            this.lblComplemento = new System.Windows.Forms.Label();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNumPC
+            // 
+            this.txtNumPC.Location = new System.Drawing.Point(58, 168);
+            this.txtNumPC.Name = "txtNumPC";
+            this.txtNumPC.Size = new System.Drawing.Size(100, 20);
+            this.txtNumPC.TabIndex = 0;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(58, 109);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.BackColor = System.Drawing.Color.Transparent;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(55, 83);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(45, 17);
+            this.lblNome.TabIndex = 2;
+            this.lblNome.Text = "Nome";
+            // 
+            // lblNumPC
+            // 
+            this.lblNumPC.AutoSize = true;
+            this.lblNumPC.BackColor = System.Drawing.Color.Transparent;
+            this.lblNumPC.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumPC.Location = new System.Drawing.Point(55, 148);
+            this.lblNumPC.Name = "lblNumPC";
+            this.lblNumPC.Size = new System.Drawing.Size(40, 17);
+            this.lblNumPC.TabIndex = 3;
+            this.lblNumPC.Text = "N PC";
+            // 
+            // chkLab
+            // 
+            this.chkLab.AutoSize = true;
+            this.chkLab.BackColor = System.Drawing.Color.Transparent;
+            this.chkLab.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkLab.Location = new System.Drawing.Point(58, 373);
+            this.chkLab.Name = "chkLab";
+            this.chkLab.Size = new System.Drawing.Size(100, 21);
+            this.chkLab.TabIndex = 4;
+            this.chkLab.Text = "Laboratório";
+            this.chkLab.UseVisualStyleBackColor = false;
+            // 
+            // lblSalas
+            // 
+            this.lblSalas.AutoSize = true;
+            this.lblSalas.BackColor = System.Drawing.Color.Transparent;
+            this.lblSalas.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSalas.Location = new System.Drawing.Point(224, 43);
+            this.lblSalas.Name = "lblSalas";
+            this.lblSalas.Size = new System.Drawing.Size(103, 39);
+            this.lblSalas.TabIndex = 5;
+            this.lblSalas.Text = "Salas";
+            // 
+            // txtNumCadeiras
+            // 
+            this.txtNumCadeiras.Location = new System.Drawing.Point(58, 231);
+            this.txtNumCadeiras.Name = "txtNumCadeiras";
+            this.txtNumCadeiras.Size = new System.Drawing.Size(100, 20);
+            this.txtNumCadeiras.TabIndex = 6;
+            // 
+            // lblNumCadeiras
+            // 
+            this.lblNumCadeiras.AutoSize = true;
+            this.lblNumCadeiras.BackColor = System.Drawing.Color.Transparent;
+            this.lblNumCadeiras.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumCadeiras.Location = new System.Drawing.Point(55, 211);
+            this.lblNumCadeiras.Name = "lblNumCadeiras";
+            this.lblNumCadeiras.Size = new System.Drawing.Size(78, 17);
+            this.lblNumCadeiras.TabIndex = 7;
+            this.lblNumCadeiras.Text = "N Cadeiras";
+            // 
+            // txtPredio
+            // 
+            this.txtPredio.Location = new System.Drawing.Point(58, 284);
+            this.txtPredio.Name = "txtPredio";
+            this.txtPredio.Size = new System.Drawing.Size(100, 20);
+            this.txtPredio.TabIndex = 8;
+            // 
+            // lblPredio
+            // 
+            this.lblPredio.AutoSize = true;
+            this.lblPredio.BackColor = System.Drawing.Color.Transparent;
+            this.lblPredio.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPredio.Location = new System.Drawing.Point(55, 264);
+            this.lblPredio.Name = "lblPredio";
+            this.lblPredio.Size = new System.Drawing.Size(49, 17);
+            this.lblPredio.TabIndex = 9;
+            this.lblPredio.Text = "Prédio";
+            // 
+            // txtComplemento
+            // 
+            this.txtComplemento.Location = new System.Drawing.Point(58, 338);
+            this.txtComplemento.Name = "txtComplemento";
+            this.txtComplemento.Size = new System.Drawing.Size(100, 20);
+            this.txtComplemento.TabIndex = 10;
+            // 
+            // lblComplemento
+            // 
+            this.lblComplemento.AutoSize = true;
+            this.lblComplemento.BackColor = System.Drawing.Color.Transparent;
+            this.lblComplemento.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblComplemento.Location = new System.Drawing.Point(55, 318);
+            this.lblComplemento.Name = "lblComplemento";
+            this.lblComplemento.Size = new System.Drawing.Size(94, 17);
+            this.lblComplemento.TabIndex = 11;
+            this.lblComplemento.Text = "Complemento";
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(53, 433);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(85, 33);
+            this.btnCadastrar.TabIndex = 12;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(163, 433);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(85, 33);
+            this.btnLimpar.TabIndex = 13;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // Salas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.Station;
+            this.ClientSize = new System.Drawing.Size(951, 672);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.lblComplemento);
+            this.Controls.Add(this.txtComplemento);
+            this.Controls.Add(this.lblPredio);
+            this.Controls.Add(this.txtPredio);
+            this.Controls.Add(this.lblNumCadeiras);
+            this.Controls.Add(this.txtNumCadeiras);
+            this.Controls.Add(this.lblSalas);
+            this.Controls.Add(this.chkLab);
+            this.Controls.Add(this.lblNumPC);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtNumPC);
+            this.Name = "Salas";
+            this.Text = "Salas";
+            this.Load += new System.EventHandler(this.Salas_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumPC;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblNumPC;
+        private System.Windows.Forms.CheckBox chkLab;
+        private System.Windows.Forms.Label lblSalas;
+        private System.Windows.Forms.TextBox txtNumCadeiras;
+        private System.Windows.Forms.Label lblNumCadeiras;
+        private System.Windows.Forms.TextBox txtPredio;
+        private System.Windows.Forms.Label lblPredio;
+        private System.Windows.Forms.TextBox txtComplemento;
+        private System.Windows.Forms.Label lblComplemento;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
---
+++ WindowsFormsApp1/Salas.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Salas : Form
+    {
+        public Salas()
+        {
+            InitializeComponent();
+        }
+
+        private void Salas_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtComplemento.Text = "";
+            txtNome.Text = "";
+            txtNumCadeiras.Text = "";
+            txtNumPC.Text = "";
+            txtPredio.Text = "";
+            chkLab.Checked = false;
+            MessageBox.Show("O formulário foi limpo com sucesso", "Mensagem");
+        }
+    }
+}
